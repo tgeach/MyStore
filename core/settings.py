29af1a3e76c5bca2de89aaabf7d7aff0833f4854
Teddie Geach <teddie@foodmesh.ca>
2022-11-21 15:46:56 -0800

    Add import-export to admin pannel

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mystore",
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = "static/"
 STATIC_URL = "static/"
 
 # Default primary key field type
--- mystore/admin.py
+++ mystore/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+from import_export.fields import Field
+from import_export.widgets import ForeignKeyWidget
 from .models import Account, Inventory, Location, Order
 # Register your models here.
 
@@ -8,7 +12,30 @@ class AccountAdmin(admin.ModelAdmin):
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ("name", "unit", "price_per_unit")
 
-class LocationAdmin(admin.ModelAdmin):
+# Adds ability to import/export data from admin pannel 
+class LocationResource(resources.ModelResource):
+    account = Field(column_name="Account Name", attribute="account", widget=ForeignKeyWidget(Account, "name"))
+    street_address = Field(attribute="street_address", column_name="Street Address")
+    
+    class Meta:
+        model = Location
+        skip_unchanged = True
+        report_skipped = True
+        feilds = (
+            "id",
+            "name",
+            "accout",
+            "street_address", 
+            "city", 
+            "province_state", 
+            "country", 
+            "zip_postal", 
+            "contact_phone"
+        )
+        
+
+
+class LocationAdmin(ImportExportModelAdmin):
     list_display = ("name", "account", "street_address", "city", "province_state", "country", "zip_postal", "contact_phone")
 
 class OrderAdmin(admin.ModelAdmin):
